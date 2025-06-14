4654552418ef7b85b2c95a12c8998a3b70bbca11
duongminhthanh <duongminhthanh1999td@gmail.com>
2025-06-14 17:23:34 +0700

    add PatientController

---
+++ patient-service/src/main/java/com/example/patientservice/controller/PatientController.java
@@ -0,0 +1,31 @@
+package com.example.patientservice.controller;
+
+import com.example.patientservice.dto.PatientRequestDTO;
+import com.example.patientservice.dto.PatientResponseDTO;
+import com.example.patientservice.service.PatientService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
+public class PatientController {
+    private final PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(
+            @Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+}
