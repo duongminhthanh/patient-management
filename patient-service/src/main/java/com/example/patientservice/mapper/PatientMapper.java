369bda937a3f2988207a21775cf9db9080aec08e
duongminhthanh <duongminhthanh1999td@gmail.com>
2025-06-14 17:22:28 +0700

    add map to model

--- patient-service/src/main/java/com/example/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/example/patientservice/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.example.patientservice.mapper;
 
+import com.example.patientservice.dto.PatientRequestDTO;
 import com.example.patientservice.dto.PatientResponseDTO;
 import com.example.patientservice.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
@@ -11,6 +14,16 @@ public class PatientMapper {
                 .address(patient.getAddress())
                 .email(patient.getEmail())
                 .dateOfBirth(patient.getDateOfBirth().toString())
+                .build();
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        return Patient.builder()
+                .name(patientRequestDTO.getName())
+                .address(patientRequestDTO.getAddress())
+                .email(patientRequestDTO.getEmail())
+                .dateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()))
+                .registeredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()))
                 .build();
     }
 }
