5abd9d95799f3c39c4775145f793fb05d5251c17
duongminhthanh <duongminhthanh1999td@gmail.com>
2025-06-24 17:47:26 +0700

    add controller

---
+++ auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.example.authservice.controller;
+
+import com.example.authservice.dto.LoginRequestDTO;
+import com.example.authservice.dto.LoginResponseDTO;
+import com.example.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
+
+        if (tokenOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+}
