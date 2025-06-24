4f222e6403a4e9caf951ac74fbe8618468d89846
duongminhthanh <duongminhthanh1999td@gmail.com>
2025-06-24 17:26:17 +0700

    add user service auth service

---
+++ auth-service/src/main/java/com/example/authservice/service/UserService.java
@@ -0,0 +1,19 @@
+package com.example.authservice.service;
+
+import com.example.authservice.model.User;
+import com.example.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
