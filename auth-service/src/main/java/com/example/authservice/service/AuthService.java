7a5142bc65ffc3f4a02e016188f0d40bf4f4edf9
duongminhthanh <duongminhthanh1999td@gmail.com>
2025-06-24 17:41:10 +0700

    add AuthService

---
+++ auth-service/src/main/java/com/example/authservice/service/AuthService.java
@@ -0,0 +1,25 @@
+package com.example.authservice.service;
+
+import com.example.authservice.dto.LoginRequestDTO;
+import com.example.authservice.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+
+        return userService.findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+    }
+}
